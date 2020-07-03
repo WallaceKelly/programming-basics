cdc6318731aea7c1b7b9fd4abe6b8d01092d288a
Wallace Kelly <wallace@bluerockresearch.com>
2020-07-03 16:15:36 -0400

    Add Python demo: combine-pdfs

---
+++ python-intro/combine-pdfs/combine-pdfs.py
@@ -0,0 +1,18 @@
+from pdfrw import PdfReader, PdfWriter
+
+fileIn1 = "Statement of Work.pdf"
+fileIn2 = "Invoice.pdf"
+fileOut = "Combined.pdf"
+
+print("Combining", fileIn1, "and", fileIn2, "into", fileOut)
+
+reader1 = PdfReader(fileIn1)
+reader2 = PdfReader(fileIn2)
+
+writer = PdfWriter()
+writer.addpages(reader1.pages)
+writer.addpages(reader2.pages)
+
+writer.write(fileOut)
+
+print("Done!")
